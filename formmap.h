d0b624882ef21a9c026b5748e454252160626fd5
rosymaro <104912704+rosymaro@users.noreply.github.com>
2023-02-21 18:48:45 +0900

    Update formmap.h

--- formmap.h
+++ formmap.h
@@ -2,10 +2,32 @@
 #define FORMMAP_H
 
 #include <QDialog>
+#include <QGraphicsRectItem>
 
 namespace Ui {
 class FormMap;
 }
+
+class SuperItem : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+public:
+    SuperItem(QGraphicsItem* parent = NULL);
+public slots:
+    void slotMove(float x, float y, float zoom, float rot, float tilt);
+
+protected:
+    void paint(QPainter *painter,
+               const QStyleOptionGraphicsItem *option,
+               QWidget *widget);
+    QRectF boundingRect() const;
+
+//    virtual void keyPressEvent(QKeyEvent *evnet);
+    float _zoom = 1;
+    float _rot = 0;
+    float _tilt = 90;
+
+};
 
 class FormMap : public QDialog
 {
@@ -14,9 +36,40 @@ class FormMap : public QDialog
 public:
     explicit FormMap(QWidget *parent = nullptr);
     ~FormMap();
+    void receiveFile(QVector<QStringList> strVector);
+
+public slots:
+    void slotInfoText(QString funcName, float value);
+
+signals:
+    void signalMove(float x, float y, float zoom, float rot, float tilt);
 
 private:
     Ui::FormMap *ui;
+    QGraphicsRectItem *rectItem;
+    QGraphicsRectItem *rectItem2;
+    QRect *rect;
+    float width;
+    float height;
+    SuperItem *super;
+
+    float pointX = 0;
+    float pointY = 0;
+    float pointZ = 0;
+    float infoTilt = 90;
+    float infoRot = 0;
+    float infoZoom = 1; //Zoom 초기값에 대한 GDS size 를 갖고와 수정 필요
+
+    float initRot = 0;
+    float initTilt = 90;
+    float initPointX = 0;
+    float initPointY = 0;
+    float initPointZ = 0;
+
+    float x = 0;
+    float y = 0;
 };
+
+
 
 #endif // FORMMAP_H
