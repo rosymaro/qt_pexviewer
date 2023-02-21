1d05ce8f22ac2e58a0ae9cec15a79486f0335a19
rosymaro <104912704+rosymaro@users.noreply.github.com>
2023-02-21 18:50:03 +0900

    Update formtop.h

--- formtop.h
+++ formtop.h
@@ -14,6 +14,7 @@ class FormTop : public QDialog
 public:
     explicit FormTop(QWidget *parent = nullptr);
     ~FormTop();
+    void receiveFile(QVector<QStringList> strVector);
 
 private:
     Ui::FormTop *ui;
