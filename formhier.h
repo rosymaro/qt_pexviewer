00985a72af72863776cf5b106792ff499d9bd217
rosymaro <104912704+rosymaro@users.noreply.github.com>
2023-02-28 18:06:57 +0900

    Update formhier.h

--- formhier.h
+++ formhier.h
@@ -2,6 +2,8 @@
 #define FORMHIER_H
 
 #include <QDialog>
+//#include "all_data.h"
+//#include "mainwindow.h"
 
 namespace Ui {
 class FormHier;
@@ -11,15 +13,22 @@ class FormHier : public QDialog
 {
     Q_OBJECT
 
-public:
-    explicit FormHier(QWidget *parent = nullptr);
-    ~FormHier();
-
 public slots:
-    void slotMove(const QPoint &, const QPoint &);
+    void ReceiveSplitData(int row, int column, const QVector <QVector <QString>> &inputDataVector);
+//    void ReceiveSplitData(QStringList list, int row, int column);
 
-private:
+public:
+    FormHier(QWidget *parent = nullptr);
+    ~FormHier();
     Ui::FormHier *ui;
+
+public:
+    //MyDataSet *dataset = nullptr;
+
+public:
+    //void setMyDataSet(MyDataSet * dataset_){ this->dataset = dataset_;}
+    //void testMyData();
+
 };
 
 #endif // FORMHIER_H
