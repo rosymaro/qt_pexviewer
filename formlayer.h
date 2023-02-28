df3a890d8683d89859a0edff4aabb5a08ef3f0f4
rosymaro <104912704+rosymaro@users.noreply.github.com>
2023-02-28 18:08:19 +0900

    Update formlayer.h

--- formlayer.h
+++ formlayer.h
@@ -2,6 +2,15 @@
 #define FORMLAYER_H
 
 #include <QDialog>
+#include <QColorDialog>
+#include <QtWidgets/QCheckBox>
+#include <QtWidgets>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QDebug>
+
 
 namespace Ui {
 class FormLayer;
@@ -11,15 +20,29 @@ class FormLayer : public QDialog
 {
     Q_OBJECT
 
+public slots:
+    void ReceiveSplitData(int row, int column, const QVector <QVector <QString>> &inputDataVector);
+
 public:
     explicit FormLayer(QWidget *parent = nullptr);
     ~FormLayer();
+    int i;
+    int row;
+    int column;
 
-public slots:
-    void slotMove(const QPoint &, const QPoint &);
+//    QTableWidgetItem *checkBoxItem[3];
+
+
+private slots:
+    void tableWidget_checkBoxChanged();
+    void on_colorbutton_clicked();
 
 private:
     Ui::FormLayer *ui;
+
+signals:
+    void outputLayerStatus(QString printLayer);
+
 };
 
 #endif // FORMLAYER_H
