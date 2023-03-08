533926f105adef99a2b2d6dd06d67c0a8d09436a
youngchul-hwang <89388094+youngchul-hwang@users.noreply.github.com>
2023-03-08 15:56:51 +0900

    Create compile.bat

---
+++ Rendering/shaders/compile.bat
@@ -0,0 +1,11 @@
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe simple_shader.vert -o simple_shader.vert.spv
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe simple_shader.frag -o simple_shader.frag.spv
+
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe simple_shader_edge.vert -o simple_shader.vert_edge.spv
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe simple_shader_edge.frag -o simple_shader.frag_edge.spv
+
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe simple_shader_pex.vert -o simple_shader.vert_pex.spv
+C:/VulkanSDK/1.3.236.0/Bin/glslc.exe simple_shader_pex.frag -o simple_shader.frag_pex.spv
+
+cmd
+pause
