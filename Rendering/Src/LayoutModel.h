6c227761b0551c9dea6b138c126ee11dab1f3575
youngchul-hwang <89388094+youngchul-hwang@users.noreply.github.com>
2023-03-08 16:15:20 +0900

    Update LayoutModel.h

---
+++ Rendering/Src/LayoutModel.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "lve_model.hpp"
+
+
+class LayoutModel : public LveModel
+{
+public:
+    LayoutModel(LveDevice& device, MODEL_TYPE model_type, const std::string& data_file);
+    ~LayoutModel();
+
+    LayoutModel() = delete;
+    LayoutModel(const LayoutModel&) = delete;
+    LayoutModel& operator=(const LayoutModel&) = delete;
+
+private:
+    LayoutDataManager layout_data;
+
+public:
+    virtual void makeRenderingData(const std::string& file_path = "");
+    virtual void loadData(const std::string& file_path = "");
+    virtual void makeVertices();
+    virtual void makeIndices();
+
+    void makeCubeVertices();
+    void makeIndicesForFace();
+    void makeIndicesForEdge();
+
+    void makeCubeVertices_map();
+    void makeIndicesForFace_map();
+    void makeIndicesForEdge_map();
+
+    LayoutDataManager* getLayoutDataManager() {return &this->layout_data;}
+
+};
+
+
+
