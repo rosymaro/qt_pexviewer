d34d9ab6ceb205adc5b96906c380da2b000f5e4e
youngchul-hwang <89388094+youngchul-hwang@users.noreply.github.com>
2023-03-09 11:35:06 +0900

    Create ui_mainwindow.h

---
+++ ui_mainwindow.h
@@ -0,0 +1,108 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGraphicsView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionOpen_file;
+    QAction *actionLayer_information;
+    QAction *actionOpen_Map_File;
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout_2;
+    QGraphicsView *graphicsView;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QMenu *menuView;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(1024, 768);
+        actionOpen_file = new QAction(MainWindow);
+        actionOpen_file->setObjectName(QString::fromUtf8("actionOpen_file"));
+        actionLayer_information = new QAction(MainWindow);
+        actionLayer_information->setObjectName(QString::fromUtf8("actionLayer_information"));
+        actionOpen_Map_File = new QAction(MainWindow);
+        actionOpen_Map_File->setObjectName(QString::fromUtf8("actionOpen_Map_File"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        verticalLayout_2 = new QVBoxLayout(centralwidget);
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        graphicsView = new QGraphicsView(centralwidget);
+        graphicsView->setObjectName(QString::fromUtf8("graphicsView"));
+
+        verticalLayout_2->addWidget(graphicsView);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 1024, 25));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        menuView = new QMenu(menubar);
+        menuView->setObjectName(QString::fromUtf8("menuView"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menuFile->menuAction());
+        menubar->addAction(menuView->menuAction());
+        menuFile->addAction(actionOpen_file);
+        menuFile->addSeparator();
+        menuFile->addAction(actionOpen_Map_File);
+        menuView->addAction(actionLayer_information);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionOpen_file->setText(QCoreApplication::translate("MainWindow", "Open file", nullptr));
+        actionLayer_information->setText(QCoreApplication::translate("MainWindow", "Layer information", nullptr));
+        actionOpen_Map_File->setText(QCoreApplication::translate("MainWindow", "Open Map File", nullptr));
+        menuFile->setTitle(QCoreApplication::translate("MainWindow", "File", nullptr));
+        menuView->setTitle(QCoreApplication::translate("MainWindow", "View", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
