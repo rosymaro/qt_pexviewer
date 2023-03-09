1da2f18e41505800d6a21e1d4bf685d0153e30d2
youngchul-hwang <89388094+youngchul-hwang@users.noreply.github.com>
2023-03-09 10:59:15 +0900

    Update formlayer.h

--- formlayer.h
+++ formlayer.h
@@ -9,6 +9,7 @@
 #include <QTableWidgetItem>
 #include <QCheckBox>
 #include <QPushButton>
+#include <QtWidgets/QSlider>
 #include <QDebug>
 
 
@@ -36,6 +37,7 @@ public:
 private slots:
     void tableWidget_checkBoxChanged();
     void on_colorbutton_clicked();
+    void handleOpacitySlider(int);
 
 private:
     Ui::FormLayer *ui;
@@ -44,5 +46,41 @@ signals:
     void outputLayerStatus(QString printLayer);
 
 };
+
+//class SliderDelegate : public QItemDelegate
+//{
+//public:
+//    SliderDelegate(QWidget *parent = nullptr) : QItemDelegate(parent){
+////        SliderDelegate delegate(this);
+//    }
+//    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,const QModelIndex &index) const override
+//    {
+//        QSlider *slider = new QSlider(Qt::Horizontal, parent);
+//        slider->setRange(0, 100);
+//        slider->setGeometry(option.rect);
+//        slider->setFocusPolicy(Qt::StrongFocus);
+//        slider->setTickPosition(QSlider::TicksBothSides);
+//        slider->setTickInterval(10);
+//        slider->show();
+//        return slider;
+//    }
+
+//    void setEditorData(QWidget *editor, const QModelIndex &index) const override
+//    {
+//        int value = index.model()->data(index, Qt::EditRole).toInt();
+//        QSlider *slider = static_cast<QSlider*>(editor);
+//        slider->setValue(value);
+//        qDebug() << slider;
+//    }
+
+//    void setModelData(QWidget *editor, QAbstractItemModel *model,
+//                      const QModelIndex &index) const override
+//    {
+//        QSlider *slider = static_cast<QSlider*>(editor);
+//        int value = slider->value();
+//        model->setData(index, value, Qt::EditRole);
+//    }
+//};
+
 
 #endif // FORMLAYER_H
